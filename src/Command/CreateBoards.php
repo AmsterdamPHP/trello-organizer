d3d501f1a2e6afae9671a84b53fedb802b218572
Rafael Dohms <rdohms@gmail.com>
2019-09-29 22:07:50 +0200

    Slack Announcement
    Announces the new Board on Slack for everyone to hop on and start doing
    chores.

--- src/Command/CreateBoards.php
+++ src/Command/CreateBoards.php
@@ -6,6 +6,8 @@ namespace AmsterdamPHP\TrelloChecklister\Command;
 use AmsterdamPHP\TrelloChecklister\ExecutionResult;
 use AmsterdamPHP\TrelloChecklister\Trello\BoardManager;
 use AmsterdamPHP\TrelloChecklister\Trello\BoardTemplate;
+use Maknz\Slack\AttachmentField;
+use Maknz\Slack\Client;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -22,9 +24,15 @@ final class CreateBoards extends Command
      */
     private $boardManager;
 
-    public function __construct(BoardManager $boardManager)
+    /**
+     * @var Client
+     */
+    private $slack;
+
+    public function __construct(BoardManager $boardManager, Client $slack)
     {
         $this->boardManager = $boardManager;
+        $this->slack        = $slack;
         parent::__construct();
     }
 
@@ -43,16 +51,25 @@ final class CreateBoards extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $template = BoardTemplate::fromYamlFile($input->getArgument('template'));
-        $result = $this->boardManager->createNextBoards($template);
+        $result   = $this->boardManager->createNextBoards($template);
 
         $this->announceNewBoardOnSlack($result);
 
         if ($result->isSuccess()) {
             $output->writeln(sprintf('<info>%s</info>', $result->getMessage()));
             $output->writeln('');
-            $output->writeln(sprintf('<comment>Board Url:</comment> %s', $result->getContext()['board_url'] ?? '-n/a-'));
-            $output->writeln(sprintf('<comment>From Template:</comment> %s', $result->getContext()['template'] ?? '-n/a-'));
-            $output->writeln(sprintf('<comment>For Meetup on:</comment> %s', $result->getContext()['date']->format(DATE_RFC3339) ?? '-n/a-'));
+            $output->writeln(
+                sprintf('<comment>Board Url:</comment> %s', $result->getContext()['board_url'] ?? '-n/a-')
+            );
+            $output->writeln(
+                sprintf('<comment>From Template:</comment> %s', $result->getContext()['template'] ?? '-n/a-')
+            );
+            $output->writeln(
+                sprintf(
+                    '<comment>For Meetup on:</comment> %s',
+                    $result->getContext()['date']->format(DATE_RFC3339) ?? '-n/a-'
+                )
+            );
         } else {
             $output->writeln(sprintf('<error>%s</error>', $result->getMessage()));
         }
@@ -60,6 +77,33 @@ final class CreateBoards extends Command
 
     private function announceNewBoardOnSlack(ExecutionResult $result): void
     {
-
+        $this->slack->attach(
+            [
+                'color' => $result->isSuccess() ? 'good':'danger',
+                'fields' => [
+                    new AttachmentField(
+                        [
+                            'title' => 'Board',
+                            'value' => $result->getContext()['board_url'] ?? 'n/a',
+                            'short' => false,
+                        ]
+                    ),
+                    new AttachmentField(
+                        [
+                            'title' => 'Template used',
+                            'value' => $result->getContext()['template'] ?? 'n/a',
+                            'short' => false,
+                        ]
+                    ),
+                    new AttachmentField(
+                        [
+                            'title' => 'Meetup',
+                            'value' => $result->getContext()['date']->format('d/F/Y') ?? 'n/a',
+                            'short' => false,
+                        ]
+                    ),
+                ],
+            ]
+        )->send($result->getMessage());
     }
 }
